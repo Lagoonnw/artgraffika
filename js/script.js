bd22efa9afbda4ce509da150ff365c868223d113
Lagoonnw <lagoonnw@gmail.com>
2016-09-22 19:02:05 +0500

    #dropmenu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var link = document.querySelector(".useful__btn-map_style");
 var map = document.querySelector(".map-js_style");
+var menuicon = document.querySelector(".header-nav__icon");
+var dropmenu = document.querySelector(".drop-down-menu");
+var close = document.querySelector(".close-menu");
  
 
 link.addEventListener("click", function (event) {
@@ -8,4 +11,22 @@ link.addEventListener("click", function (event) {
  
 });
 
+menuicon.addEventListener("click", function (event) {
+  event.preventDefault();
+  dropmenu.classList.add("drop-down-menu_style-show");
+});
+
+close.addEventListener("click", function (event) {
+  event.preventDefault();
+  dropmenu.classList.remove("drop-down-menu_style-show");   
+ 
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (dropmenu.classList.contains("drop-down-menu_style-show")) {
+      dropmenu.classList.remove("drop-down-menu_style-show");
+    }
+  }
+})
 
